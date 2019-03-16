e2ecade1f8b87c45d92d32d5bd8041fd2ab9bcf7
thinlizzy <jose.diego@gmail.com>
2019-03-16 18:32:37 -0400

    fixed Point operator* and added scalar()

--- die-tk/objects/Point.h
+++ die-tk/objects/Point.h
@@ -35,14 +35,20 @@ public:
 	friend constexpr basic_point operator+(basic_point const & p1, basic_point const & p2) { return basic_point(p1)+=p2; }
 	constexpr basic_point & operator-=(basic_point const & p) { x -= p.x;  y -= p.y; return *this; }
 	friend constexpr basic_point operator-(basic_point const & p1, basic_point const & p2) { return basic_point(p1)-=p2; }
-	constexpr basic_point & operator*=(T f) { x *= f;  y *= f; return *this; }
-	constexpr basic_point & operator*=(basic_point const & p) { x *= p.x;  y *= p.y; return *this; }
-	friend constexpr basic_point operator*(basic_point const & p, T f) { return basic_point(p)*=f; }
-	friend constexpr basic_point operator*(T f, basic_point const & p) { return basic_point(p)*=f; }
-	friend constexpr basic_point operator*(basic_point const & p1, basic_point const & p2) { return basic_point(p1)*=p2; }
+	template<typename U>
+	constexpr basic_point & operator*=(U f) { x *= f;  y *= f; return *this; }
+	template<typename U>
+	friend constexpr basic_point operator*(basic_point const & p, U f) { return basic_point(p)*=f; }
+	template<typename U>
+	friend constexpr basic_point operator*(U f, basic_point const & p) { return basic_point(p)*=f; }
 	constexpr basic_point & operator/=(T f) { x /= f;  y /= f; return *this; }
 	friend constexpr basic_point operator/(basic_point const & p, T f) { return basic_point(p)/=f; }
-    
+
+	template<typename U>
+	constexpr basic_point scalar(U sx, U sy) const { return basic_point(x*sx,y*sy); }
+	template<typename U>
+	constexpr basic_point scalar(basic_point<U> const & p) const { return scalar(p.x,p.y); }
+
     constexpr T distance() const { return std::sqrt(x*x+y*y); }
 
 	friend std::ostream & operator<<(std::ostream & os, basic_point const & p) { os << '(' << p.x << ',' << p.y << ')'; return os; }
