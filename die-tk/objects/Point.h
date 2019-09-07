828ca23e898f07c9c9e33a36eef0d802a1319725
thinlizzy <jose.diego@gmail.com>
2019-09-06 21:59:03 -0400

    removed "friend" member functions from Point due to weirdness with templates

--- die-tk/objects/Point.h
+++ die-tk/objects/Point.h
@@ -32,17 +32,10 @@ public:
     constexpr bool operator==(basic_point const & p) const { return p.x == x && p.y == y; }
     constexpr bool operator!=(basic_point const & p) const { return ! operator==(p); }
 	constexpr basic_point & operator+=(basic_point const & p) { x += p.x;  y += p.y; return *this; }
-	friend constexpr basic_point operator+(basic_point const & p1, basic_point const & p2) { return basic_point(p1)+=p2; }
 	constexpr basic_point & operator-=(basic_point const & p) { x -= p.x;  y -= p.y; return *this; }
-	friend constexpr basic_point operator-(basic_point const & p1, basic_point const & p2) { return basic_point(p1)-=p2; }
 	template<typename U>
 	constexpr basic_point & operator*=(U f) { x *= f;  y *= f; return *this; }
-	template<typename U>
-	friend constexpr basic_point operator*(basic_point const & p, U f) { return basic_point(p)*=f; }
-	template<typename U>
-	friend constexpr basic_point operator*(U f, basic_point const & p) { return basic_point(p)*=f; }
 	constexpr basic_point & operator/=(T f) { x /= f;  y /= f; return *this; }
-	friend constexpr basic_point operator/(basic_point const & p, T f) { return basic_point(p)/=f; }
 
 	template<typename U>
 	constexpr basic_point scalar(U sx, U sy) const { return basic_point(x*sx,y*sy); }
@@ -53,6 +46,20 @@ public:
 
 	friend std::ostream & operator<<(std::ostream & os, basic_point const & p) { os << '(' << p.x << ',' << p.y << ')'; return os; }
 };
+
+template<typename T>
+constexpr basic_point<T> operator+(basic_point<T> const & p1, basic_point<T> const & p2) { return basic_point<T>(p1)+=p2; }
+
+template<typename T>
+constexpr basic_point<T> operator-(basic_point<T> const & p1, basic_point<T> const & p2) { return basic_point<T>(p1)-=p2; }
+
+template<typename T, typename U>
+constexpr basic_point<T> operator*(basic_point<T> const & p, U f) { return basic_point<T>(p)*=f; }
+template<typename T, typename U>
+constexpr basic_point<T> operator*(U f, basic_point<T> const & p) { return basic_point<T>(p)*=f; }
+template<typename T>
+constexpr basic_point<T> operator/(basic_point<T> const & p, T f) { return basic_point<T>(p)/=f; }
+
 
 using Point = basic_point<int>;
 
