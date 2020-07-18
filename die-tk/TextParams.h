cebe17879041d8a6b6ad18d989e7c1ec389d7d87
thinlizzy <jose.diego@gmail.com>
2020-07-18 14:43:26 -0400

    fixed TextParams default values to make more sense

--- die-tk/TextParams.h
+++ die-tk/TextParams.h
@@ -19,8 +19,7 @@ public:
 	TextParams():
 		h_align(HTextAlign::left),
 		v_align(VTextAlign::center),
-		textColor(0,0,0),
-		backgroundColor({255,255,255})
+		textColor(255,255,255)
 	{}
 	TextParams & horizontalAlign(HTextAlign align) { h_align = align; return *this; }
 	TextParams & verticalAlign(VTextAlign align) { v_align = align; return *this; }
